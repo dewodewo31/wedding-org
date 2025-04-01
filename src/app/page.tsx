38d9ca631891684d15dd8eb3c040070601aab6ad
dewodewo31 <dewodewo3104@gmail.com>
2025-04-02 04:58:26 +0800

    Feat: Implementation Design of Feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Header from "@/components/Header";
 import WeddingPackages from "@/components/WeddingPackages";
+import Thumbsup from "@/app/assets/images/thumbsup.svg";
+import Creditcard from "@/app/assets/images/creditcard.svg";
+import Place from "@/app/assets/images/place.svg";
+import Link from "next/link";
+
 
 export default function Home() {
   return (
@@ -7,6 +12,51 @@ export default function Home() {
       <Header />
       <section className="">
       <WeddingPackages show="popular" type="slider"/>
+      </section>
+
+      <section className="container mx-auto flex flex-col">
+        <h2 className="text-3xl font-bold max-w-md mx-auto text-center mb-8">
+          Alasan Mereka Memilih Wedding Package Samawa
+        </h2>
+        <div className="grid grid-cols-3 gap-10">
+          <div className="flex flex-col border rounded-3xl p-8 gap-y-5 items-start">
+            <span className="text-color2">
+              <Thumbsup />
+            </span>
+            <span className="flex flex-col gap-y-2">
+              <h6 className="font-bold text-xl">Dipercaya Sejak 1970</h6>
+              <p className="">
+                Lorem ipsum dolor si amet nikah berkah dunia akhirat nantinya
+              </p>
+            </span>
+            <Link href="#" className="text-color2 hover:underline">Learn More</Link>
+          </div>
+          <div className="flex flex-col border rounded-3xl p-8 gap-y-5 items-start">
+            <span className="text-color2">
+            <Creditcard />
+            </span>
+            <span className="flex flex-col gap-y-2">
+              <h6 className="font-bold text-xl">Dipercaya Sejak 1970</h6>
+              <p className="">
+                Lorem ipsum dolor si amet nikah berkah dunia akhirat nantinya
+              </p>
+            </span>
+            <Link href="#" className="text-color2 hover:underline">Learn More</Link>
+          </div>
+
+          <div className="flex flex-col border rounded-3xl p-8 gap-y-5 items-start">
+            <span className="text-color2">
+              <Place/>
+            </span>
+            <span className="flex flex-col gap-y-2">
+              <h6 className="font-bold text-xl">Dipercaya Sejak 1970</h6>
+              <p className="">
+                Lorem ipsum dolor si amet nikah berkah dunia akhirat nantinya
+              </p>
+            </span>
+            <Link href="#" className="text-color2 hover:underline">Learn More</Link>
+          </div>
+        </div>
       </section>
     </main>
   );
